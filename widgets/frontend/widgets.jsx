1aa31c3bd894de4dcdd29f02b7da6711a1cec44a
le-s <steven.an.le@gmail.com>
2018-11-30 14:12:06 -0800

    Setup widget and add CSS warmup

---
+++ widgets/frontend/widgets.jsx
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   const root = document.getElementById('root');
+// 
+//   ReactDOM.render(
+//     <h1>React is working</h1>,
+//     root
+//   );
+// });
